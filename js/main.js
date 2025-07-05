ab869df729469288fa207be254ae315a7cdb3839
左右丞聯盟 <leftrightchancellor@gmail.com>
2025-07-05 17:19:53 +0800

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,17 +1,18 @@
 $(()=>
     {
         const gsat = new Date( 2026 , 1 , 17 , 8 ) ; 
-        const now = new Date() ; 
-        const y = now.getFullYear() ; 
-        const m = String( now.getMonth() + 1 ).padStart( 2 , "0" ) ; 
-        const d = String( now.getDate() ).padStart( 2 , "0" ) ; 
+        let now = new Date() ; 
         function updateLocalTime() 
         {
+            now = new Date() ; 
+            const y = now.getFullYear() ; 
+            const m = String( now.getMonth() + 1 ).padStart( 2 , "0" ) ; 
+            const d = String( now.getDate() ).padStart( 2 , "0" ) ; 
             const weekday = now.toLocaleDateString( "en-US", { weekday : "long" } ) ; 
             const hms = now.toLocaleTimeString( "en-US", { hour12 : false } ) ; 
             $( "#now" ).text( `${y}-${m}-${d} ${hms} (${weekday})` ) ; 
+            $( "#day" ).text( Math.round( ( gsat - now ) / ( 1000 * 60 * 60 * 24 ) ) ) ; 
         }
-        $( "#day" ).text( Math.round( ( gsat - now ) / ( 1000 * 60 * 60 * 24 ) ) ) ; 
-        _updateTimeId = setInterval( updateLocalTime , 1000 ) ; 
+        const a = setInterval( updateLocalTime , 1000 ) ; 
         updateLocalTime() ; 
     });
