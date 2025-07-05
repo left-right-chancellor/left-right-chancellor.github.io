40197a60fe0f25cd1d0ebe9cdb73d2e42ccbab27
左右丞聯盟 <leftrightchancellor@gmail.com>
2025-07-05 16:38:10 +0800

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,4 +1,21 @@
 $(()=>
     {
-        
+        const gsat = new Date( 2026 , 1 , 17 , 8 ) ; 
+        const now = new Date() ; 
+        const y = now.getFullYear() ; 
+        const m = String( now.getMonth() + 1 ).padStart( 2 , "0" ) ; 
+        const d = String( now.getDate() ).padStart( 2 , "0" ) ; 
+        function updateLocalTime() 
+        {
+            const weekday = now.toLocaleDateString( "en-US", { weekday : "long" } ) ; 
+            const hms = now.toLocaleTimeString( "en-US", { hour12 : false } ) ; 
+            const localEl = $( "#now" ) ; 
+            if( localEl ) 
+            {
+                localEl.textContent = `${y}-${m}-${d} ${hms} (${weekday})` ; 
+            }
+        }
+        $( "#day" ).text( Math.round( ( gsat - now ) / ( 1000 * 60 * 60 * 24 ) ) ; 
+        _updateTimeId = setInterval( updateLocalTime , 1000 ) ; 
+        updateLocalTime() ; 
     });
