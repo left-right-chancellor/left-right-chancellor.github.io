2dc1dbbb97316531253023d0a914ff08d705be1a
JeansLo <louischo303@gmail.com>
2025-07-31 18:12:44 +0800

    Add 阮宥翔生日

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 $(()=>
     {
         const gsat = new Date( 2026 , 0 , 17 , 8 ) ; 
+        const 阮宥翔生日 = new Date( 2026 , 0 , 16 ) ; 
         let now = new Date() ; 
         function updateLocalTime() 
         {
@@ -14,11 +15,19 @@ $(()=>
             const hr = Math.floor( ( gsat - now ) / ( 1000 * 60 * 60 ) ) - day * 24 ; 
             const min = Math.floor( ( gsat - now ) / ( 1000 * 60 ) ) - Math.floor( ( gsat - now ) / ( 1000 * 60 * 60 ) ) * 60 ; 
             const sec = Math.floor( ( gsat - now ) / 1000 ) - Math.floor( ( gsat - now ) / ( 1000 * 60 ) ) * 60 + 1 ; 
+            const Bday = Math.floor( ( 阮宥翔生日 - now ) / ( 1000 * 60 * 60 * 24 ) ) ; 
+            const Bhr = Math.floor( ( 阮宥翔生日 - now ) / ( 1000 * 60 * 60 ) ) - day * 24 ; 
+            const Bmin = Math.floor( ( 阮宥翔生日 - now ) / ( 1000 * 60 ) ) - Math.floor( ( 阮宥翔生日 - now ) / ( 1000 * 60 * 60 ) ) * 60 ; 
+            const Bsec = Math.floor( ( 阮宥翔生日 - now ) / 1000 ) - Math.floor( ( 阮宥翔生日 - now ) / ( 1000 * 60 ) ) * 60 + 1 ; 
             $( "#now" ).text( `民國${y}年${m}月${d}日（${weekday}） ${hms}` ) ; 
             $( "#day" ).text( day ) ; 
             $( "#hr" ).text( String( hr ).padStart( 2 , "0" ) ) ; 
             $( "#m" ).text( String( min ).padStart( 2 , "0" ) ) ; 
             $( "#s" ).text( String( sec == 60 ? 0 : sec ).padStart( 2 , "0" ) ) ; 
+            $( "#Bday" ).text( Bday ) ; 
+            $( "#Bhr" ).text( String( Bhr ).padStart( 2 , "0" ) ) ; 
+            $( "#Bm" ).text( String( Bmin ).padStart( 2 , "0" ) ) ; 
+            $( "#Bs" ).text( String( Bsec == 60 ? 0 : Bsec ).padStart( 2 , "0" ) ) ; 
         }
         const a = setInterval( updateLocalTime , 500 ) ; 
         updateLocalTime() ; 
