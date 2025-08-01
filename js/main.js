de5fe939725c235ddbe853ed9fadf77f6e9ed35c
JeansLo <louischo303@gmail.com>
2025-08-01 23:16:00 +0800

    Add 分科倒數

--- js/main.js
+++ js/main.js
@@ -1,7 +1,10 @@
 $(()=>
     {
-        const gsat = new Date( 2026 , 0 , 17 , 8 ) ; 
+        const 學測 = new Date( 2026 , 0 , 17 , 8 ) ; 
+        const 分科 = new Date( 2026 , 5 , 3 , 8 ) ; 
         const 阮宥翔生日 = new Date( 2026 , 0 , 16 ) ; 
+        const 天差 = Math.floor( ( 學測 - 阮宥翔生日 ) / ( 1000 * 60 * 60 * 24 ) ) ; 
+        const 時差 = Math.floor( ( 學測 - 阮宥翔生日 ) / ( 1000 * 60 * 60 ) ) - 天差 * 24 ; 
         let now = new Date() ; 
         function updateLocalTime() 
         {
@@ -11,23 +14,27 @@ $(()=>
             const d = String( now.getDate() ).padStart( 2 , "0" ) ; 
             const weekday = now.toLocaleDateString( "zh-TW", { weekday : "long" } ) ; 
             const hms = now.toLocaleTimeString( "en-US", { hour12 : false } ) ; 
-            const day = Math.floor( ( gsat - now ) / ( 1000 * 60 * 60 * 24 ) ) ; 
-            const hr = Math.floor( ( gsat - now ) / ( 1000 * 60 * 60 ) ) - day * 24 ; 
-            const min = Math.floor( ( gsat - now ) / ( 1000 * 60 ) ) - Math.floor( ( gsat - now ) / ( 1000 * 60 * 60 ) ) * 60 ; 
-            const sec = Math.floor( ( gsat - now ) / 1000 ) - Math.floor( ( gsat - now ) / ( 1000 * 60 ) ) * 60 + 1 ; 
-            const Bday = Math.floor( ( 阮宥翔生日 - now ) / ( 1000 * 60 * 60 * 24 ) ) ; 
-            const Bhr = Math.floor( ( 阮宥翔生日 - now ) / ( 1000 * 60 * 60 ) ) - Bday * 24 ; 
-            const Bmin = Math.floor( ( 阮宥翔生日 - now ) / ( 1000 * 60 ) ) - Math.floor( ( 阮宥翔生日 - now ) / ( 1000 * 60 * 60 ) ) * 60 ; 
-            const Bsec = Math.floor( ( 阮宥翔生日 - now ) / 1000 ) - Math.floor( ( 阮宥翔生日 - now ) / ( 1000 * 60 ) ) * 60 + 1 ; 
+            const day = Math.floor( ( 學測 - now ) / ( 1000 * 60 * 60 * 24 ) ) ; 
+            const hr = Math.floor( ( 學測 - now ) / ( 1000 * 60 * 60 ) ) - day * 24 ; 
+            const min = Math.floor( ( 學測 - now ) / ( 1000 * 60 ) ) - Math.floor( ( 學測 - now ) / ( 1000 * 60 * 60 ) ) * 60 ; 
+            const sec = Math.floor( ( 學測 - now ) / 1000 ) - Math.floor( ( 學測 - now ) / ( 1000 * 60 ) ) * 60 + 1 ; 
+            const Aday = Math.floor( ( 分科 - now ) / ( 1000 * 60 * 60 * 24 ) ) ; 
+            const Ahr = Math.floor( ( 分科 - now ) / ( 1000 * 60 * 60 ) ) - Aday * 24 ; 
+            const Amin = Math.floor( ( 分科 - now ) / ( 1000 * 60 ) ) - Math.floor( ( 分科 - now ) / ( 1000 * 60 * 60 ) ) * 60 ; 
+            const Asec = Math.floor( ( 分科 - now ) / 1000 ) - Math.floor( ( 分科 - now ) / ( 1000 * 60 ) ) * 60 + 1 ; 
             $( "#now" ).text( `民國${y}年${m}月${d}日（${weekday}） ${hms}` ) ; 
             $( "#day" ).text( day ) ; 
             $( "#hr" ).text( String( hr ).padStart( 2 , "0" ) ) ; 
             $( "#m" ).text( String( min ).padStart( 2 , "0" ) ) ; 
             $( "#s" ).text( String( sec == 60 ? 0 : sec ).padStart( 2 , "0" ) ) ; 
-            $( "#Bday" ).text( Bday ) ; 
-            $( "#Bhr" ).text( String( Bhr ).padStart( 2 , "0" ) ) ; 
-            $( "#Bm" ).text( String( Bmin ).padStart( 2 , "0" ) ) ; 
-            $( "#Bs" ).text( String( Bsec == 60 ? 0 : Bsec ).padStart( 2 , "0" ) ) ; 
+            $( "#Aday" ).text( Aday ) ; 
+            $( "#Ahr" ).text( String( Ahr ).padStart( 2 , "0" ) ) ; 
+            $( "#Am" ).text( String( Amin ).padStart( 2 , "0" ) ) ; 
+            $( "#As" ).text( String( Asec == 60 ? 0 : Asec ).padStart( 2 , "0" ) ) ; 
+            $( "#Bday" ).text( day - 天差 ) ; 
+            $( "#Bhr" ).text( String( 時差 - hr ).padStart( 2 , "0" ) ) ; 
+            $( "#Bm" ).text( String( min ).padStart( 2 , "0" ) ) ; 
+            $( "#Bs" ).text( String( sec == 60 ? 0 : sec ).padStart( 2 , "0" ) ) ; 
         }
         const a = setInterval( updateLocalTime , 500 ) ; 
         updateLocalTime() ; 
